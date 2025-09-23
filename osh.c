6c236d3c16d44cac856e2e7f40a5fcbd164f4c37
oveljosland <oveljosland@icloud.com>
2025-09-23 13:37:23 +0200

    add splitline

--- osh.c
+++ osh.c
@@ -4,7 +4,7 @@
 #define EXIT_FAIL 1
 
 void loop(void);
-
+char *readline(void);
 
 int main(int argc, char **argv)
 {
@@ -29,7 +29,7 @@ void loop(void)
 
 	do {
 		printf("$ ");
-		line = _readline();
+		line = readline();
 		args = splitline(line);
 		status = exec(args);
 
@@ -38,4 +38,63 @@ void loop(void)
 	} while (status);
 }
 
+#define BUFSIZ (1<<10)
+char *readline(void)
+{
+	int i, c, bufsiz = BUFSIZ;
+	char *buf = malloc(sizeof(char) * bufsiz);
+	
+	if (!buf) {
+		fprintf(stderr, "osh: alloc error\n");
+		exit(EXIT_FAIL);
+	}
 
+	i = 0;
+	while (1) { /* basically 'getline' */
+		if ((c = getchar()) == EOF || c == '\n')
+			buf[i] = '\0'; return  buf;
+		else
+			buf[i] = c;
+		i++;
+
+		if (i >= bufsiz) {
+			buf += BUFSIZE;
+			buf = realloc(buf, bufsiz);
+			if (!buf) {
+				fprintf(stderr, "osh: alloc error\n");
+				exit(EXIT_FAIL);
+			}
+		}
+	}
+}
+
+#define TOK_BUFSIZ (1<<6)
+#define TOK_DELIM " \t\r\n\a"
+char **splitline(char *line)
+{
+	int bufsiz = TOK_BUFSIZ, i = 0;
+	char **toks = malloc(sizeof(char*) * bufsiz);
+	char *tok;
+
+	if (!toks) {
+		fprintf(stderr, "osh: alloc error\n");
+		exit(EXIT_FAIL);
+	}
+
+	tok = strtok(line, TOK_DELIM);
+	while (tok != NULL) {
+		toks[i] = tok;
+		i++;
+		if (i >= bufsiz) {
+			bufsiz += TOK_BUFSIZ;
+			toks = realloc(toks, sizeof(char*) * bufsiz);
+			if (!toks) {
+				fprintf(stderr, "osh: alloc error\n");
+				exit(EXIT_FAIL);
+			}
+		}
+		tok = strtok(NULL, TOK_DELIM);
+	}
+	toks[i] = NULL;
+	return toks;
+}
